57b93f4777e28455168920e7e16443de23ed9478
miRaccle-16 <miracleugbana5@gmail.com>
2026-05-25 10:59:26 +0100

    DB_SSL update in env

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ const db = mysql.createConnection({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'ani_beauty_db',
-    port: 3306
+    port: process.env.DB_PORT || 3306,
+    ssl:process.env.DB_SSL==='true' ? {rejectUnauthorized:false} : false
+
 });
 
 db.connect((err) => {
